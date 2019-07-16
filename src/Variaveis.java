3ad3d74e135008adc294aaf8727759321f1b674b
joaodematejr <joaodematejr@gmail.com>
2019-07-15 21:47:21 -0300

    aula4

--- src/Variaveis.java
+++ src/Variaveis.java
@@ -3,7 +3,10 @@ package src;
 public class Variaveis {
     // ENTRY POINT
     public static void main(String[] args) {
-        System.out.println("Olá Mundo !");
+        System.out.println("Inicio do Programa");
+        // TIPO DO DADO //NOME DA VARIAVEL / = / VALOR DA VARIAVEL
+        Integer idade = 25;
+        System.out.println(idade);
     }
 
 }
