7bb8f929e523306f36fa8ecfd1fd374a1a10e40c
joaodematejr <joaodematejr@gmail.com>
2019-07-15 21:37:45 -0300

    aula 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,4 @@
-package src.aula_1;
+package src;
 
 public class HelloWorld {
 
---
+++ src/Variaveis.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Variaveis {
+    // ENTRY POINT
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo !");
+    }
+
+}
