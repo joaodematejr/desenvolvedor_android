d68e4a56e27f71f262ada90263764bb4becf0aff
joaodematejr <joaodematejr@gmail.com>
2019-07-22 21:19:33 -0300

    Padrão ViewHolder

--- src/Apps/ConversorMoedas/app/src/main/java/com/joaodematejr/conversormoedas/MainActivity.java
+++ src/Apps/ConversorMoedas/app/src/main/java/com/joaodematejr/conversormoedas/MainActivity.java
@@ -3,12 +3,35 @@ package com.joaodematejr.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText valor = findViewById(R.id.edit_value);
+        valor.getText();
+
+        //VIEWHOLDER
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+    }
+    
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
